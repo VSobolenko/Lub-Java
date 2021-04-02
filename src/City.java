a838690cf1d0b57ef8b2639cd8c6dd618a60232b
Vladislav Sobolenko <v.sobolenko.vs@gmail.com>
2021-04-02 20:46:47 +0300

    make constructor

---
+++ src/City.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+
+public class City {
+    private final String name;
+    private final int population;
+    private final int square;
+
+    public City(String name, int population){
+        this.name = name;
+        this.population = population;
+        square = 10000;
+    }
+    public City(String name, int population, int square){
+        this.name = name;
+        this.population = population;
+        this.square = square;
+    }
+
+    public City() {
+        name = "default";
+        population = 0;
+        square = 0;
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+
 public class Runner {
     //IDZ 1 - variant 16 - class name City
+    public static void main(String args[]){
+
+    }
 }
