712dc1d4d07474bd55189735e8414840197c4764
Vladislav Sobolenko <v.sobolenko.vs@gmail.com>
2021-04-02 20:48:18 +0300

    override comparator

--- src/City.java
+++ src/City.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-public class City {
+public class City implements Comparable<City> {
     private final String name;
     private final int population;
     private final int square;
@@ -20,6 +20,44 @@ public class City {
         name = "default";
         population = 0;
         square = 0;
+    }
+
+    public static Comparator<City> NameComparator = new Comparator<City>() {
+        @Override
+        public int compare(City o1, City o2) {
+            if(o1 == null || o2 == null){
+                return 0;
+            }
+            return o1.name.compareTo(o2.name);
+        }
+    };
+    public static Comparator<City> PopulationComparator = new Comparator<City>() {
+        @Override
+        public int compare(City o1, City o2) {
+            if(o1 == null || o2 == null){
+                return 0;
+            }
+            return o1.population - o2.population;
+        }
+    };
+    public static Comparator<City> SquareComparator = new Comparator<City>() {
+        @Override
+        public int compare(City o1, City o2) {
+            if(o1 == null || o2 == null){
+                return 0;
+            }
+            return o1.square - o2.square;
+        }
+    };
+
+    @Override
+    public String toString() {
+        return "[name=" + name + " population=" + population + " square=" + square + "]";
+    }
+
+    @Override
+    public int compareTo(City o) {
+        return 0;
     }
 
     public String getName() {
