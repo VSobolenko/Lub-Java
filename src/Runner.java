70c2f065dbead1e06c1c5dac5b27a8bfa0d8eb00
Vladislav Sobolenko <v.sobolenko.vs@gmail.com>
2021-04-04 14:58:31 +0300

    main class

--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,15 @@
+import by.gsu.asoilab.*;
+
 public class Runner {
     public static void main(String args[]){
+        Material steel = new Material("steel", 7.7f);
+        Subject wire = new Subject("wire",steel, 0.03f);
+        System.out.println(wire.toString());
+
+        wire.setMaterial(new Material("copper", 8.5f));
+        System.out.println(wire.getMass());
+
+        wire.setMaterial(steel);
+        System.out.println(wire.toString());
     }
 }
