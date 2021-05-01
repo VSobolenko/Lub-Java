d28da7c19c2d3e0ed8618c6ff28814021bde7460
Vladislav Sobolenko <v.sobolenko.vs@gmail.com>
2021-05-01 17:29:41 +0300

    add String s1 and s2

--- src/Runner.java
+++ src/Runner.java
@@ -11,6 +11,11 @@ import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Какую строку надо заменить?");
+        String s1 = in.nextLine();
+        System.out.println("На какую строку нужно заменить?");
+        String s2 = in.nextLine();
 
     }
 }
