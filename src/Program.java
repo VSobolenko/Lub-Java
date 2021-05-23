91b0cf0114939593ed53fe0d2f803d2cb0721c45
Vladislav Sobolenko <v.sobolenko.vs@gmail.com>
2021-05-24 00:07:27 +0300

    add exception OutOfMemoryError

--- src/Program.java
+++ src/Program.java
@@ -2,34 +2,39 @@ import java.io.*;
 
 public class Program {
     public static void main(String args[]) throws IOException, ClassNotFoundException {
-        int i = 0;
-        String value;
-        CitySer[] citySer = new CitySer[10];
-        CityExtern[] cityExtern =  new CityExtern[10];
+        try {
+            int i = 0;
+            String value;
+            CitySer[] citySer = new CitySer[10];
+            CityExtern[] cityExtern =  new CityExtern[10];
 
-        //Читать данные с файла и записать их в массив
-        try (BufferedReader reader = new BufferedReader(new FileReader("src\\in.txt")))  {
-            value = reader.readLine();
-            while (value != null){
-                citySer[i] = GetCitySer(value);
-                cityExtern[i] = GetCityExtern(value);
+            //Читать данные с файла и записать их в массив
+            try (BufferedReader reader = new BufferedReader(new FileReader("src\\in.txt")))  {
                 value = reader.readLine();
-                i++;
+                while (value != null){
+                    citySer[i] = GetCitySer(value);
+                    cityExtern[i] = GetCityExtern(value);
+                    value = reader.readLine();
+                    i++;
+                }
             }
+            catch(IOException ex){
+                System.out.println(ex.getMessage());
+            }
+
+            Task.Serializable(citySer);
+
+            Task.Externalizable(cityExtern);
+
+            cityExtern = null;
+            citySer = null;
+
+            citySer = Task.DeSerializable();
+            cityExtern = Task.DeExternalizable();
         }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
+        catch (OutOfMemoryError e){
+            System.out.println(e.getMessage());
         }
-
-        Task.Serializable(citySer);
-
-        Task.Externalizable(cityExtern);
-
-        cityExtern = null;
-        citySer = null;
-
-        citySer = Task.DeSerializable();
-        cityExtern = Task.DeExternalizable();
     }
 
     public static CitySer GetCitySer(String csvCity){
